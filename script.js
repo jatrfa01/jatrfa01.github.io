0779491b5edc3ad8a0f672c5d51613f4d7463089
jatrfa01 <jatrfa01@luther.edu>
2016-04-13 22:45:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
     //url: 'https://ru.wikipedia.org/w/api.php?format=json&action=mobileview&redirects&prop=sections|normalizedtitle&sectionprop=toclevel|line|index&page=%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8F%20(%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)',
     url: 'http://en.wikipedia.org/w/api.php?action=parse&page=' + inputText + '&prop=text&format=json&callback=?'
     //url: 'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=Main%20Page&prop=revisions&rvprop=content&callback=?',
-    dataType: 'jsonp'
+    //dataType: 'jsonp'
     }).done(function (data) {
     console.log(data);
     if (data.error) {
