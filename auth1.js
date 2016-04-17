fd23a4578116c78cba34081f95d6f65b2c2499cd
jatrfa01 <jatrfa01@luther.edu>
2016-04-17 18:49:11 -0500

    Update auth1.js

--- auth1.js
+++ auth1.js
@@ -2,7 +2,7 @@
 // at https://console.developers.google.com/.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
-var OAUTH2_CLIENT_ID = '__YOUR_CLIENT_ID__';
+var OAUTH2_CLIENT_ID = '35495291351-ra6mrknhkg244ntrecu7qgd0tkno3pt4.apps.googleusercontent.com	';
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
